66bcdbdfd65aafe9840637cba5e21788efb965de
Ming Hsu <minghsu@profcoo.com>
2023-07-22 17:02:27 +0800

    feat: add problem 1

---
+++ problems/p1-data-types/main.js
@@ -0,0 +1,20 @@
+let value = 0;
+console.log(typeof value, value);
+
+value = 'string';
+console.log(typeof value, value);
+
+value = true;
+console.log(typeof value, value);
+
+value = { a: 0, b: 1 };
+console.log(typeof value, value);
+
+value = [0, 1];
+console.log(typeof value, value);
+
+value = null;
+console.log(typeof value, value);
+
+value = undefined;
+console.log(typeof value, value);
